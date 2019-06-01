adfa56684c7a5b9e3fea2b620fe761cff9b7d440
Helene Juterud <hjuterud@gmail.com>
2019-06-01 04:09:04 +0200

    Add Description component for result item

---
+++ src/components/ResultItemDescription/ResultItemDescription.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const resultItemDescription = props => {
+  return (
+    <p>
+    {props.location.Description}
+    </p>
+  );
+
+}
+
+export default resultItemDescription;
--- src/containers/ResultItemContainer/ResultItemContainer.js
+++ src/containers/ResultItemContainer/ResultItemContainer.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ResultItem from '../../components/ResultItem/ResultItem.js';
+import ResultItemDescription from '../../components/ResultItemDescription/ResultItemDescription.js';
 
 const resultItemContainer =  props => {
 
   let isDescriptionToggled = false;
 
- const  toggleExpandDescView = () => {
-   isDescriptionToggled = !isDescriptionToggled;
-   console.log(isDescriptionToggled);
- };
+ const  toggleExpandDescView = () => isDescriptionToggled = !isDescriptionToggled;
+
   return (
       <div className="resultItem--container">
-        <ResultItem location={props.location} toggle={toggleExpandDescView}/>
+      {isDescriptionToggled ? <ResultItemDescription  location={props.location}/>  :
+        <ResultItem location={props.location} toggle={toggleExpandDescView}/> }
       </div>
   );
 
