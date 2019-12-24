0cd66cb9c76f7f588045732eef4e3c3f04721ec5
Helene Juterud <hjuterud@gmail.com>
2019-12-24 13:19:09 +0100

    Make nav icons and nav section smaller

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ import './Navbar.css';
 const navbar = ({optionChosen, changeMenuOption}) =>{
   return(
       <nav className="navbar--content">
-        <div className={optionChosen === 'PVE' ? 'navbar--item navbar--item-active' :'navbar--item' } onClick={() => changeMenuOption('PVE')}>
-          <i className="fas fa-globe-europe fa-2x"></i>PvE
+        <div className={optionChosen === 'PVE' ? 'navbar--item navbar--item-active' : 'navbar--item' } onClick={() => changeMenuOption('PVE')}>
+          <i className="fas fa-globe-europe"></i>PvE
         </div>
-        <div className={optionChosen === 'FRACTAL' ? 'navbar--item navbar--item-active' :'navbar--item' } onClick={() => changeMenuOption('FRACTAL')}>
-          <i className="fas fa-dungeon fa-2x"></i>Fractals
+        <div className={optionChosen === 'FRACTAL' ? 'navbar--item navbar--item-active' : 'navbar--item' } onClick={() => changeMenuOption('FRACTAL')}>
+          <i className="fas fa-dungeon"></i>Fractals
         </div>
-        <div className={optionChosen === 'WVW' ? 'navbar--item navbar--item-active' :'navbar--item' } onClick={() => changeMenuOption('WVW')}>
-          <i className="fas fa-chess-rook fa-2x"></i>WvW
+        <div className={optionChosen === 'WVW' ? 'navbar--item navbar--item-active' : 'navbar--item' } onClick={() => changeMenuOption('WVW')}>
+          <i className="fas fa-chess-rook"></i>WvW
         </div>
       </nav>
   );
