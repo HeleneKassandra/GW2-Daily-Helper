234dd844ea792e973172ec3bce1f2e1ada2d5df9
HeleneKassandra <hjuterud@gmail.com>
2023-07-02 20:10:10 +0200

    update dependencies

--- rewrite/gw2-daily-helper/lib/sanityClient.js
+++ rewrite/gw2-daily-helper/lib/sanityClient.js
@@ -1,9 +1,8 @@
 // client.js
-import sanityClient from '@sanity/client'
+import {createClient} from '@sanity/client'
 
-export default sanityClient({
+export default createClient({
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production', // or the name you chose in step 1,
     apiVersion: '2021-10-21',
-    useCdn: true // `false` if you want to ensure fresh data
 })
